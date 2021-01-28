d343e95ce2846d1b29bfd898ff8fdcc133dda519
DanzelPOYU <b6894121@gmail.com>
2021-01-28 13:31:07 +0800

    testpyopencv

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
-img = cv2.imread('test.png',0)
+img = cv2.imread('test.png',1)
+
+variablename = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 
 print(img)
 # cv2.namedWindow('test',cv2.WINDOW_NORMAL)
@@ -9,6 +11,6 @@ print(img)
 # cv2.imwrite('test1.jpg',img)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
-plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+plt.imshow(img)
 plt.xticks([]), plt.yticks([])
 plt.show()
